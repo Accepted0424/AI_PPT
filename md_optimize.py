4ec757b50c3ae2f3641f28ad8e37bf0d2d054b42
lsyycf <lsyycf@outlook.com>
2024-11-30 10:37:00 +0800

    清理无用文件

--- md_optimize.py
+++ md_optimize.py
@@ -2,47 +2,6 @@ import re
 from getPicture import call_image_downloader
 
 
-def get_optimize_md(md_path):
-    # config
-    new_content = ('template: Martin Template.pptx\n'
-                   + 'cardlayout: horizontal\n'
-                   + 'baseTextSize: 20\n'
-                   + 'CardColour: BACKGROUND 2\n'
-                   + 'CardTitlePosition: inside\n'
-                   + 'cardshadow: yes\n'
-                   + 'cardshape: rounded\n\n')
-
-    with open(md_path, 'r+', encoding='utf-8') as f:
-        content = f.read()
-        new_content += content
-        f.seek(0)
-        f.write(new_content)
-        f.truncate()
-        # 获取图片关键词，删去关键词，增加图片链接
-        # for line in content.split('\n'):
-        #    if line.startswith('* (') or line.startswith('(') or line.startswith('  * ('):
-        #       pic_query_list = re.findall(r'\((.*?)\)', line)
-        #       pic_query = pic_query_list[0]
-        #        try:
-        #            pattern = r'optimize/content_(\d+)\.md'
-        #            match = re.match(pattern, md_path)
-        #            i = int(match.group(1))
-        #            call_image_downloader(pic_query, f"picture/part_{i}")
-        #            image_path = f"picture/part_{i}/{pic_query}.png"
-        #        except:
-        #            pass
-        #        new_line = f"![{pic_query}](.\\{image_path})\n"
-        #        new_content += new_line
-        #    elif line.startswith('---'):
-        #        break
-        #    else:
-        #        new_content += line
-        #        new_content += '\n'
-        # f.seek(0)
-        # f.write(new_content)
-        # f.truncate()
-
-
 def get_optimize_md_with_img(md_path):
     # config
     new_content = ('template: Martin Template.pptx\n'
