dcce9bc7ce86ec749cf5733b78333cfc24151b73
罗浩宇 <23373112@buaa.edu.cn>
2024-12-06 09:56:09 +0800

    add background

--- md_optimize.py
+++ md_optimize.py
@@ -1,7 +1,6 @@
 import re
 from getPicture import call_image_downloader
 
-
 def get_optimize_md_with_img(md_path):
     # config
     new_content = ('template: Martin Template.pptx\n'
@@ -10,13 +9,16 @@ def get_optimize_md_with_img(md_path):
                    + 'CardColour: BACKGROUND 2\n'
                    + 'CardTitlePosition: inside\n'
                    + 'cardshadow: yes\n'
-                   + 'cardshape: rounded\n\n')
+                   + 'cardshape: rounded\n'
+                   + 'removeFirstSlide: yes\n'
+                   + 'backgroundImage: background.png\n'
+                   + 'marginBase: 0.5\n\n')
 
     with open(md_path, 'r+', encoding='utf-8') as f:
         content = f.read()
         # 获取图片关键词，删去关键词，增加图片链接
         for line in content.split('\n'):
-            if line.startswith('* (') or line.startswith('(') or line.startswith('  * ('):
+            if line.startswith('* (') or line.startswith('(') or line.startswith('  * (') or line.startswith('  (') or line.startswith('    * (') or line.startswith(' ('):
                 pic_query_list = re.findall(r'\((.*?)\)', line)
                 pic_query = pic_query_list[0]
                 try:
